69a56c614aba24a2cfbf7adc2b7d5db4758e49be
h0lycats <99686554+h0lycats@users.noreply.github.com>
2022-05-19 23:21:00 +0400

    Update Main.java

--- Krotkov_Java_Array/src/Main.java
+++ Krotkov_Java_Array/src/Main.java
@@ -1,19 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите числа для внесения в массив, разделив их запятой или пробелом");
+            String input = scanner.nextLine();
+            input = input.trim();
 
-        List<Integer> integers = new ArrayList<>();
-        integers.add(3);
-        integers.add(7);
-        integers.add(21);
-        integers.add(8);
-        printMultipliesOfThree(integers);
+            printMultipliesOfThree(getListOfIntegers(input));
 
+        }
     }
 
     private static void printMultipliesOfThree(List<Integer> listOfIntegers) {
@@ -23,6 +27,17 @@ public class Main {
 
 
     }
+
+    private static List <Integer> getListOfIntegers (String input){
+        List <Integer> integerList = new ArrayList<>();
+        String regex = "([\\d]+)[\\,]*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+            integerList.add(Integer.parseInt(matcher.group(1)));
+        }
+    return integerList;}
 
 }
 
